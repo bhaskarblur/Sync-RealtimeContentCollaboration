8d460d483b57e063b55490315ac190b5591567e0
Devajit <patar1447@gmail.com>
2024-01-15 21:05:31 +0530

    Adding functionality direct intent to DocumentActivity after creating new document

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
+    id("com.android.application") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.dagger.hilt.android") version "2.46" apply false
 }
